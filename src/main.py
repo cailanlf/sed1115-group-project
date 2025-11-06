c13a014bc9b11b166544f678b0ef844997878786
cailanlf <40549567+cailanlf@users.noreply.github.com>
2025-11-06 16:01:08 -0500

    add button for pen control

--- src/main.py
+++ src/main.py
@@ -5,8 +5,15 @@ from time import time_ns
 pot_pin_x = ADC(Pin(27))
 pot_pin_y = ADC(Pin(26))
 
+btn_pin = Pin(12)
+btn_debounce = 50.
+
 def main():
     poll_timer = 0
+    btn_debounce_timer = 0
+    pen_state = False
+
+    btn_last_state = 0
 
     start = time_ns()
     while True:
@@ -16,13 +23,21 @@ def main():
         start = end
 
         poll_timer += elapsed
+        btn_debounce_timer += elapsed
         
         # read and print the potentiometer values every 50 ms
         if poll_timer > 100:
             poll_timer = 0
             x = pot_pin_x.read_u16()
             y = pot_pin_y.read_u16()
-            print(f"\r{x: <5}, {y: <5}", end="")
+            print(f"\r{x: <5}, {y: <5} pen={'DOWN' if pen_state else 'UP  '}", end="")
+
+        # check if button is pressed and debounce appropriately
+        if btn_pin.value() and not btn_last_state and btn_debounce_timer > btn_debounce:
+            pen_state = not pen_state
+            btn_debounce_timer = 0
+
+        btn_last_state = btn_pin.value()
 
 if __name__ == "__main__":
     main()
