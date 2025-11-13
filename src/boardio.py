67ca6be8637f1abe4eba2fb8588c2ddd933493ba
cailanlf <40549567+cailanlf@users.noreply.github.com>
2025-11-13 16:19:32 -0500

    update skeleton; extract state to classes

---
+++ src/boardio.py
@@ -0,0 +1,93 @@
+from machine import ADC, Pin
+
+class PotentiometerState:
+    """
+    Handles periodically reading from the potentiometers and caching the values.
+    """
+    pot_x: ADC
+    pot_y: ADC
+
+    x_value: float
+    y_value: float
+    
+    pot_poll_interval: float
+    timer: float
+
+    def __init__(self, x_pin: int, y_pin: int, pot_poll_interval: float):
+        self.pot_x = ADC(Pin(x_pin))
+        self.pot_y = ADC(Pin(y_pin))
+
+        self.pot_poll_interval = pot_poll_interval
+        self.timer = 0
+
+    def _read(self):
+        """
+        Read the values from the potentiometers and update the internal state.
+        """
+
+        # read the values and convert them to floats
+        x = self.pot_x.read_u16() / 65535.0
+        y = self.pot_y.read_u16() / 65535.0
+
+        # clamp the x and y to 0.0 - 1.0 (just in case)
+        self.x_value = 0. if x < 0. else 1. if x > 1. else x
+        self.y_value = 0. if y < 0. else 1. if y > 1. else y
+
+    def get(self) -> 'tuple[float, float]':
+        """
+        Get the cached x, y values as a tuple
+        """
+        return self.x_value, self.y_value
+
+    def update(self, elapsed_time: float):
+        self.timer += elapsed_time
+
+        if self.timer >= self.pot_poll_interval:
+            self.timer = 0
+            self._read()
+
+class ButtonState:
+    """
+    Handles reading from and debouncing a button and caches the button's toggle state.
+    """
+    _btn: Pin
+    _btn_debounce: float
+
+    _timer: float
+
+    _toggled_on: bool
+
+    _btn_last_pressed: bool
+
+    def __init__(self, btn_pin: int, btn_debounce: float):
+        self._btn = Pin(btn_pin)
+        self._btn_debounce = btn_debounce
+        self.timer = 0
+        self._toggled_on = False
+        self._btn_last_pressed = False
+
+    def get(self) -> bool:
+        """
+        Retrieve whether the button is currently toggled or not.
+        """
+        return self._toggled_on
+
+    def update(self, elapsed_time: float) -> None:
+        """
+        Update the state with the elapsed time.
+        """
+        self.elapsed_time += elapsed_time
+
+        # get the button's current pressed state
+        btn_pressed = self._btn.value()
+
+        # debounce 
+        if btn_pressed and not self._btn_last_pressed \
+                and self.elapsed_time >= self._btn_debounce:
+            self.elapsed_time = 0
+            self._toggled_on = not self._toggled_on
+        
+        self._btn_last_pressed = btn_pressed
+
+class ArmController:
+    def __init__(self, shoulder_pin: int, elbow_pin: int, wrist_pin: int):
--- src/main.py
+++ src/main.py
@@ -1,19 +1,49 @@
-from machine import Pin, ADC
 from time import time_ns
 
-# Configuration
-pot_pin_x = ADC(Pin(27))
-pot_pin_y = ADC(Pin(26))
+from boardio import PotentiometerState, ButtonState, ArmController
 
-btn_pin = Pin(12)
+# Configuration
+pot_pin_x = 27
+pot_pin_y = 26
+pot_poll_interval = 50
+
+# the pin to use for the button
+btn_pin = 12
+
+# the time in ms to debounce btn
 btn_debounce = 50.
 
-def main():
-    poll_timer = 0
-    btn_debounce_timer = 0
-    pen_state = False
+# pins for PWM output to arm
+shoulder_pin = 0
+elbow_pin = 1
+wrist_pin = 2
 
-    btn_last_state = 0
+def solve_kinematics(x: float, y: float) -> 'tuple[float, float] | None':
+    """
+    Get a solution of (alpha, beta) in degrees to move the arm to the specified position.
+    Returns None if there is no solution.
+    """
+    raise NotImplementedError()
+
+def convert_board_coordinates(x: float, y: float) -> 'tuple[float, float]':
+    """
+    Convert the given [0 - 1] x, y coordinates into coordinates on the board.
+    """
+    raise NotImplementedError()
+
+def get_actual_angles(arm: 'ArmController') -> 'tuple[float, float]':
+    """
+    Read the actual angles from the arm and return them.
+
+    Returns:
+        A tuple representing the angles that the arm and shoulder are actually at.
+    """
+    raise NotImplementedError()
+
+def main():
+    potentiometer_states = PotentiometerState(pot_pin_x, pot_pin_y, pot_poll_interval)
+    button_state = ButtonState(btn_pin, btn_debounce)
+    arm_controller = ArmController(shoulder_pin=shoulder_pin, elbow_pin=elbow_pin, wrist_pin=wrist_pin)
 
     start = time_ns()
     while True:
@@ -22,23 +52,33 @@ def main():
         elapsed = (end - start) / 1_000_000
         start = end
 
-        poll_timer += elapsed
-        btn_debounce_timer += elapsed
+        # update the handlers with the elapsed time
+        potentiometer_states.update(elapsed)
+        button_state.update(elapsed)
+
+        # get the values from the handlers
+        x, y = potentiometer_states.get()
+        pen_down = button_state.get()
+
+        print(x, y, f"x: {x:.f4}, y: {y:.f4}, pen: {'down' if pen_down else 'up'}")
+
+        # convert x, y to board coordinates for the arm
+        board_x, board_y = convert_board_coordinates(x, y)
+
+        # solve the inverse kinematics equations
+        kinematics_solution = solve_kinematics(x, y)
+
+        if kinematics_solution is None:
+            # handle this
+            raise NotImplementedError()
+            continue
         
-        # read and print the potentiometer values every 50 ms
-        if poll_timer > 100:
-            poll_timer = 0
-            x = pot_pin_x.read_u16()
-            y = pot_pin_y.read_u16()
-            print(f"\r{x: <5}, {y: <5} pen={'DOWN' if pen_state else 'UP  '}", end="")
+        alpha, beta = kinematics_solution
+        
+        arm_controller.set_arm_angles(alpha, beta)
+        arm_controller.set_wrist_down(pen_down)
 
-        # check if button is pressed and debounce appropriately
-        if btn_pin.value() and not btn_last_state and btn_debounce_timer > btn_debounce:
-            pen_state = not pen_state
-            btn_debounce_timer = 0
-
-        btn_last_state = btn_pin.value()
+        error_shoulder, error_arm = get_actual_angles(arm_controller)
 
 if __name__ == "__main__":
     main()
-
